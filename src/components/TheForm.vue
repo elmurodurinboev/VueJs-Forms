4f63f5a0d084cf3ba6d63b3d6f3732a3720cc0e8
elmurodWeb <elmurodorinboyev@gmail.com>
2023-09-08 16:03:03 +0500

    Add Vmodel into each input item. Add validation function to the username

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,16 +1,25 @@
 <template>
-  <form>
-    <div class="form-control">
+  <form @submit.prevent="formData">
+    <div class="form-control" :class="{ invalid: isNameInvalid === 'invalid' }">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model="inputName"
+        @blur="inputValidate"
+      />
+      <p v-if="isNameInvalid === 'invalid'">
+        UserName is invalid. Plaese check the username!
+      </p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
-      <input id="age" name="age" type="number" />
+      <input id="age" name="age" type="number" v-model.trim="inputAge" />
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -19,38 +28,122 @@
     <div class="form-control">
       <h2>What are you interested in?</h2>
       <div>
-        <input id="interest-news" name="interest" type="checkbox" />
+        <input
+          id="interest-news"
+          name="interest"
+          type="checkbox"
+          value="news"
+          v-model="interest"
+        />
         <label for="interest-news">News</label>
       </div>
       <div>
-        <input id="interest-tutorials" name="interest" type="checkbox" />
+        <input
+          id="interest-tutorials"
+          name="interest"
+          type="checkbox"
+          value="tutorials"
+          v-model="interest"
+        />
         <label for="interest-tutorials">Tutorials</label>
       </div>
       <div>
-        <input id="interest-nothing" name="interest" type="checkbox" />
+        <input
+          id="interest-nothing"
+          name="interest"
+          type="checkbox"
+          value="nothing"
+          v-model="interest"
+        />
         <label for="interest-nothing">Nothing</label>
       </div>
     </div>
     <div class="form-control">
       <h2>How do you learn?</h2>
       <div>
-        <input id="how-video" name="how" type="radio" />
+        <input
+          id="how-video"
+          name="how"
+          type="radio"
+          v-model="how"
+          value="video"
+        />
         <label for="how-video">Video Courses</label>
       </div>
       <div>
-        <input id="how-blogs" name="how" type="radio" />
+        <input
+          id="how-blogs"
+          name="how"
+          type="radio"
+          v-model="how"
+          value="tutorials"
+        />
         <label for="how-blogs">Blogs</label>
       </div>
       <div>
-        <input id="how-other" name="how" type="radio" />
+        <input
+          id="how-other"
+          name="how"
+          type="radio"
+          v-model="how"
+          value="other"
+        />
         <label for="how-other">Other</label>
       </div>
+    </div>
+
+    <div class="form-control">
+      <input type="checkbox" name="terms" id="terms" v-model="terms" />
+      <label for="terms">Agree with terms and conditions</label>
     </div>
     <div>
       <button>Save Data</button>
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputName: '',
+      inputAge: null,
+      referrer: 'wom',
+      interest: [],
+      how: '',
+      terms: false,
+      isNameInvalid: 'pending',
+    };
+  },
+
+  methods: {
+    formData() {
+      console.log(this.inputName);
+      this.inputName = '';
+
+      console.log(this.inputAge);
+      this.inputAge = null;
+
+      console.log(this.referrer);
+      this.referrer = 'wom';
+
+      console.log(this.interest);
+      console.log(this.how);
+      (this.interest = []), (this.how = '');
+
+      console.log(this.terms);
+      this.terms = false;
+    },
+    inputValidate() {
+      if (this.inputName === '') {
+        this.isNameInvalid = 'invalid';
+      } else {
+        this.isNameInvalid = 'valid';
+      }
+    },
+  },
+};
+</script>
 
 <style scoped>
 form {
@@ -64,6 +157,15 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid p,
+.form-control.invalid label {
+  color: red;
 }
 
 label {
